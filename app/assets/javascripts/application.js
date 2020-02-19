70820d12cc5451a6de1cc05d68f38e19795b2d08
minh bui <tranduyminh.bui@gmail.com>
2020-02-19 19:28:37 +1100

    finish AirBnB clone for Rails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,41 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+ // TODO: Write your JS code in here
+
+
+const myAPI = 'pk.eyJ1IjoibWluaGJ1aSIsImEiOiJjazRhcWRkcWcwNjRvM2V1cTcxcGc1c2M1In0.QePhyDBj0T5pwR0ibkTdTw';
+
+const baseUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/';
+
+let flatAddress, url, map
+
+let displayMap = (coor) => {
+  mapboxgl.accessToken = myAPI;
+
+  map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9',
+    center: coor,
+    zoom: 12
+  });
+
+  new mapboxgl.Marker()
+    .setLngLat(coor)
+    .addTo(map);
+};
+
+
+let getCoordinates = (newAddress) => {
+  flatAddress = newAddress.split(' ').join('%20');
+
+  url = `${baseUrl}${flatAddress}.json?access_token=${myAPI}`;
+
+  fetch(url)
+    .then(res => res.json())
+    .then((data) => {
+      console.log(data.features[0].center);
+      displayMap(data.features[0].center);
+    });
+};
